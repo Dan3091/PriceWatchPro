1df0d78a75c50cabc78173f7b399d29d58fdd62a
Dan <clarc.2007@gmail.com>
2024-01-31 17:58:26 +0200

    Added all necessary labels

--- gui.py
+++ gui.py
@@ -36,3 +36,71 @@ class App(Tk):
                         bg="#ff9900",
                         highlightthickness=0)
         canvas.pack()
+
+        # Added all necessary labels and entries
+        self.label_product = Label(self,
+                                   text="Product Item Name:",
+                                   font="Italic 14",
+                                   bg="#131921",
+                                   fg="white")
+        self.label_product.pack(pady=(20, 0))
+        self.label_product_error = Label(self,
+                                         text="",
+                                         font="Italic 10",
+                                         bg="#131921",
+                                         fg="red")
+        self.label_product_error.pack()
+        self.product_entry = Entry(self, font="Italic 14", bg="#232f3e", fg="white")
+        self.product_entry.pack()
+
+        self.label_price = Label(self,
+                                 text="Max Price:",
+                                 font="Italic 14",
+                                 bg="#131921",
+                                 fg="white")
+        self.label_price.pack()
+        self.label_price_error = Label(self,
+                                       text="",
+                                       font="Italic 10",
+                                       bg="#131921",
+                                       fg="red")
+        self.label_price_error.pack()
+        self.price_entry = Entry(self, font="Italic 14", bg="#232f3e", fg="white")
+        self.price_entry.pack()
+
+        self.label_subtitle = Label(self,
+                                    text="Now  enter your email credencial\n to receive the notification:",
+                                    font="Italic 12",
+                                    bg="#131921",
+                                    fg="white")
+        self.label_subtitle.pack(pady=(20, 10))
+
+        self.label_email = Label(self,
+                                 text="Email:",
+                                 font="Italic 14",
+                                 bg="#131921",
+                                 fg="white")
+        self.label_email.pack()
+        self.label_email_error = Label(self,
+                                       text="",
+                                       font="Italic 10",
+                                       bg="#131921",
+                                       fg="red")
+        self.label_email_error.pack()
+        self.email_entry = Entry(self, font="Italic 14", bg="#232f3e", fg="white")
+        self.email_entry.pack()
+
+        self.label_password = Label(self,
+                                    text="Password:",
+                                    font="Italic 14",
+                                    bg="#131921",
+                                    fg="white")
+        self.label_password.pack()
+        self.label_password_error = Label(self,
+                                          text="",
+                                          font="Italic 10",
+                                          bg="#131921",
+                                          fg="red")
+        self.label_password_error.pack()
+        self.password_entry = Entry(self, font="Italic 14", bg="#232f3e", fg="white", show="*")
+        self.password_entry.pack()
