494ab1c95edd2b019aad82a16b69119a98904b03
Dan <clarc.2007@gmail.com>
2024-01-31 17:31:20 +0200

    Created a class App inherited from Tk class from tkinter module

--- gui.py
+++ gui.py
@@ -4,3 +4,16 @@ from pystray import MenuItem as item
 import pystray
 
 
+class App(Tk):
+    """
+    This class is used to define the main window by using tkinter graphic interface module.
+    """
+
+    def __init__(self, start_process):
+        self.start_process = start_process
+
+        super().__init__()
+        self.title("PriceWatchPro")
+        self.resizable(False, False)
+        self.configure(bg="#131921")
+        self.overrideredirect(True)
